c6c49d00ffeba86c0b5d5e6e6c75b6555f37ff42
Skole <Skole>
2017-04-21 08:29:22 +0200

    commit

---
+++ Object.cpp
@@ -0,0 +1,100 @@
+#include "Object.h"
+
+Object::Object()
+{
+	//set values for default constructor
+	setType("Object");
+	setColor(Scalar(0,0,0));
+
+}
+
+Object::Object(string name){
+
+	setType(name);
+	
+	if(name=="green"){
+
+		//TODO: use "calibration mode" to find HSV min
+		//and HSV max values
+
+		setHSVmin(Scalar(34,50,50));
+		setHSVmax(Scalar(102,179,200));
+
+		//BGR value for Green:
+		setColor(Scalar(0,255,0));
+
+	}
+	if(name=="black"){
+
+		//TODO: use "calibration mode" to find HSV min
+		//and HSV max values
+
+		setHSVmin(Scalar(0,0,0));
+		setHSVmax(Scalar(170,256,60));
+
+		//BGR value for Black:
+		setColor(Scalar(0,0,0));
+
+	}
+	if(name=="red"){
+
+		//TODO: use "calibration mode" to find HSV min
+		//and HSV max values
+
+		setHSVmin(Scalar(0,21,0));
+		setHSVmax(Scalar(18,255,255));
+
+		//BGR value for Red:
+		setColor(Scalar(0,0,255));
+
+	}
+}
+
+Object::~Object(void)
+{
+}
+
+int Object::getXPos(){
+
+	return Object::xPos;
+
+}
+
+void Object::setXPos(int x){
+
+	Object::xPos = x;
+
+}
+
+int Object::getYPos(){
+
+	return Object::yPos;
+
+}
+
+void Object::setYPos(int y){
+
+	Object::yPos = y;
+
+}
+
+Scalar Object::getHSVmin(){
+
+	return Object::HSVmin;
+
+}
+Scalar Object::getHSVmax(){
+
+	return Object::HSVmax;
+}
+
+void Object::setHSVmin(Scalar min){
+
+	Object::HSVmin = min;
+}
+
+
+void Object::setHSVmax(Scalar max){
+
+	Object::HSVmax = max;
+}
